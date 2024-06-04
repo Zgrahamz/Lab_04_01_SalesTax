dd5dabd768f6134d4d62423be688b1efa906f40b
ZGrahamz <graham_zach@outlook.com>
2024-06-03 20:42:28 -0400

    Message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        int pPrice = 10;
+        double sTax = 0.05;
+
+        System.out.println("Purchase price is " + pPrice);
+        System.out.println("Sales tax price is " + (pPrice * sTax));
     }
 }
